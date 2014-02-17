f42f419ad4c9474947a8e553af27167d157c15ab
Joan Puigcerver <joapuipe@gmail.com>
2014-02-17 13:43:38 +0100

    Bug fixed. Rprop, SteepestDescent and Matrix stylized

--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -15,128 +15,133 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with RNNLIB.  If not, see <http://www.gnu.org/licenses/>.*/
 
-#ifndef _INCLUDED_Matrix_h  
-#define _INCLUDED_Matrix_h  
+#ifndef _INCLUDED_Matrix_h
+#define _INCLUDED_Matrix_h
 
 #define OP_TRACKING
 
 #ifdef OP_TRACKING
-static unsigned long long matrixOps = 0;
+static uint64_t matrixOps = 0;
 #endif
 
-// M += a * b 
-static void outer(const real_t *aBegin, const real_t *aEnd, real_t *M, const real_t *b, const real_t *bEnd)
-{
+// M += a * b
+static void outer(
+    const real_t *aBegin, const real_t *aEnd, real_t *M, const real_t *b,
+    const real_t *bEnd) {
 #ifdef OP_TRACKING
-	const real_t* mStart = M;
+  const real_t* mStart = M;
 #endif
-	for (;b != bEnd; ++b)
-	{
-		real_t input = *b;
-		for (const real_t *a = aBegin; a != aEnd; ++a, ++M)
-		{
-			*M += *a * input;
-		}
-	}
+  for (; b != bEnd; ++b) {
+    real_t input = *b;
+    for (const real_t *a = aBegin; a != aEnd; ++a, ++M) {
+      *M += *a * input;
+    }
+  }
 #ifdef OP_TRACKING
-	matrixOps += M - mStart;
-#endif
-}
-//out += M in
-static void dot(const real_t *inBegin, const real_t *inEnd, const real_t *M, real_t *out, real_t *outEnd)
-{
-#ifdef OP_TRACKING
-	const real_t* mStart = M;
-#endif
-	for (;out != outEnd; ++out)
-	{
-		real_t sum = 0;
-		for (const real_t *in = inBegin; in != inEnd; ++in, ++M)
-		{ 
-			sum += *M * (*in);
-		}
-		*out += sum; 
-	}
-#ifdef OP_TRACKING
-	matrixOps += M - mStart;
+  matrixOps += M - mStart;
 #endif
 }
-//out += transpose(M) in
-static void dot_transpose(const real_t *in, const real_t *inEnd, const real_t *M, real_t *outBegin, real_t *outEnd)
-{
+
+// out += M in
+static void dot(
+    const real_t *inBegin, const real_t *inEnd, const real_t *M, real_t *out,
+    real_t *outEnd) {
 #ifdef OP_TRACKING
-	const real_t* mStart = M;
+  const real_t* mStart = M;
 #endif
-	for (;in != inEnd; ++in)
-	{
-		real_t input = *in;
-		for (real_t *out = outBegin; out != outEnd; ++out, ++M)
-		{
-			*out += *M * input;
-		}
-	}
+  for (; out != outEnd; ++out) {
+    real_t sum = 0;
+    for (const real_t *in = inBegin; in != inEnd; ++in, ++M) {
+      sum += *M * (*in);
+    }
+    *out += sum;
+  }
 #ifdef OP_TRACKING
-	matrixOps += M - mStart;
-#endif
-}
-//out += transpose(M^2) in
-static void dot_transpose_m_squared(const real_t *in, const real_t *inEnd, const real_t *M, real_t *outBegin, real_t *outEnd)
-{
-#ifdef OP_TRACKING
-	const real_t* mStart = M;
-#endif
-	for (;in != inEnd; ++in)
-	{
-		real_t input = *in;
-		for (real_t *out = outBegin; out != outEnd; ++out, ++M)
-		{
-			*out += squared(*M) * input;
-		}
-	}
-#ifdef OP_TRACKING
-	matrixOps += M - mStart;
+  matrixOps += M - mStart;
 #endif
 }
-// M += a^2 * b 
-static void outer_a_squared(const real_t *aBegin, const real_t *aEnd, real_t *M, const real_t *b, const real_t *bEnd)
-{
+
+// out += transpose(M) in
+static void dot_transpose(
+    const real_t *in, const real_t *inEnd, const real_t *M, real_t *outBegin,
+    real_t *outEnd) {
 #ifdef OP_TRACKING
-	const real_t* mStart = M;
+  const real_t* mStart = M;
 #endif
-	for (;b != bEnd; ++b)
-	{
-		real_t input = *b;
-		for (const real_t *a = aBegin; a != aEnd; ++a, ++M)
-		{
-			*M += squared(*a) * input;
-		}
-	}
+  for (; in != inEnd; ++in) {
+    real_t input = *in;
+    for (real_t *out = outBegin; out != outEnd; ++out, ++M) {
+      *out += *M * input;
+    }
+  }
 #ifdef OP_TRACKING
-	matrixOps += M - mStart;
+  matrixOps += M - mStart;
 #endif
 }
-template<class R> static void outer(const R& a, real_t *M, const R&b)
-{
-	outer(boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
+
+// out += transpose(M^2) in
+static void dot_transpose_m_squared(
+    const real_t *in, const real_t *inEnd, const real_t *M, real_t *outBegin,
+    real_t *outEnd) {
+#ifdef OP_TRACKING
+  const real_t* mStart = M;
+#endif
+  for (; in != inEnd; ++in) {
+    real_t input = *in;
+    for (real_t *out = outBegin; out != outEnd; ++out, ++M) {
+      *out += squared(*M) * input;
+    }
+  }
+#ifdef OP_TRACKING
+  matrixOps += M - mStart;
+#endif
 }
-template<class R> static void dot(const R& a, const real_t *M, const R& b)
-{
-	dot(boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
+
+// M += a^2 * b
+static void outer_a_squared(
+    const real_t *aBegin, const real_t *aEnd, real_t *M, const real_t *b,
+    const real_t *bEnd) {
+#ifdef OP_TRACKING
+  const real_t* mStart = M;
+#endif
+  for (; b != bEnd; ++b) {
+    real_t input = *b;
+    for (const real_t *a = aBegin; a != aEnd; ++a, ++M) {
+      *M += squared(*a) * input;
+    }
+  }
+#ifdef OP_TRACKING
+  matrixOps += M - mStart;
+#endif
 }
-template<class R> static void dot_transpose(const R& a, const real_t *M, const R& b)
-{
-	dot_transpose(boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
+
+template<class R> static void outer(const R& a, real_t *M, const R&b) {
+  outer(boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
 }
-template<class R> static void outer_a_squared(const R& a, real_t *M, const R&b)
-{
-	outer_a_squared(boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
+
+template<class R> static void dot(const R& a, const real_t *M, const R& b) {
+  dot(boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
 }
-template<class R> static void dot_transpose_m_squared(const R& a, const real_t *M, const R& b)
-{
-	dot_transpose_m_squared(boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
+
+template<class R> static void dot_transpose(
+    const R& a, const real_t *M, const R& b) {
+  dot_transpose(
+      boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
 }
-static real_t& elt(View<real_t> M, int x, int y, int width)
-{
-	return M[(y*width) + x];
+
+template<class R> static void outer_a_squared(
+    const R& a, real_t *M, const R&b) {
+  outer_a_squared(
+      boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
+}
+
+template<class R> static void dot_transpose_m_squared(
+    const R& a, const real_t *M, const R& b) {
+  dot_transpose_m_squared(
+      boost::begin(a), boost::end(a), M, boost::begin(b), boost::end(b));
+}
+
+static real_t& elt(View<real_t> M, int x, int y, int width) {
+  return M[(y*width) + x];
 }
 #endif
--- src/SteepestDescent.hpp
+++ src/SteepestDescent.hpp
@@ -15,69 +15,66 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with RNNLIB.  If not, see <http://www.gnu.org/licenses/>.*/
 
-#ifndef _INCLUDED_SteepestDescent_h  
+#ifndef _INCLUDED_SteepestDescent_h
 #define _INCLUDED_SteepestDescent_h
 
 #include <algorithm>
-#include "Optimiser.hpp"
+#include <string>
+#include <vector>
+
 #include "DataExporter.hpp"
+#include "Optimiser.hpp"
+
 
 using namespace std;
 extern bool verbose;
 
-struct SteepestDescent: public DataExporter, public Optimiser
-{
-	//data
-	ostream& out;
-	vector<real_t> deltas;
-	real_t learnRate;
-	real_t momentum;
-	
-	//functions
-	SteepestDescent(const string& name, ostream& o, vector<real_t>& weights, vector<real_t>& derivatives, 
-			real_t lr = 1e-4, real_t mom = 0.9):
-		DataExporter(name),
-		Optimiser(weights, derivatives),
-		out(o),
-		learnRate(lr),
-		momentum(mom)
-	{
-		build();
-	}
-	void update_weights()
-	{
-		assert(wts.size() == derivs.size());
-		assert(wts.size() == deltas.size());
-        LOOP (int i, indices(wts))
-        {
-            real_t delta = (momentum * deltas[i]) - (learnRate * derivs[i]);
-            deltas[i] = delta;
-            wts[i] += delta;
-        }
-		if (verbose)
-		{
-			out << this->name << " weight updates:" << endl;
-			PRINT(minmax(wts), out);
-			PRINT(minmax(derivs), out);
-			PRINT(minmax(deltas), out);
-		}
-	}
-	//NOTE must be called after any change to weightContainer
-	void build()
-	{
-		if (deltas.size() != wts.size())
-		{		
-			deltas.resize(wts.size());
-			fill(deltas, 0);
-			WeightContainer::instance().save_by_conns(deltas, name + "_deltas");
-		}
-	}
-	void print(ostream& out = cout) const
-	{
-		out << "steepest descent" << endl;
-		PRINT(learnRate, out);
-		PRINT(momentum, out);
-	}
+struct SteepestDescent: public DataExporter, public Optimiser {
+  // data
+  ostream& out;
+  vector<real_t> deltas;
+  real_t learnRate;
+  real_t momentum;
+
+  // functions
+  SteepestDescent(
+      const string& name, ostream& o, vector<real_t>& weights,
+      vector<real_t>& derivatives, real_t lr = 1e-4, real_t mom = 0.9):
+      DataExporter(name), Optimiser(weights, derivatives), out(o),
+      learnRate(lr), momentum(mom) {
+    build();
+  }
+
+  void update_weights() {
+    assert(wts.size() == derivs.size());
+    assert(wts.size() == deltas.size());
+    LOOP(int i, indices(wts)) {
+      real_t delta = (momentum * deltas[i]) - (learnRate * derivs[i]);
+      deltas[i] = delta;
+      wts[i] += delta;
+    }
+    if (verbose) {
+      out << this->name << " weight updates:" << endl;
+      PRINT(minmax(wts), out);
+      PRINT(minmax(derivs), out);
+      PRINT(minmax(deltas), out);
+    }
+  }
+
+  // NOTE must be called after any change to weightContainer
+  void build() {
+    if (deltas.size() != wts.size()) {
+      deltas.resize(wts.size());
+      fill(deltas, 0);
+      WeightContainer::instance().save_by_conns(deltas, name + "_deltas");
+    }
+  }
+
+  void print(ostream& out = cout) const {
+    out << "steepest descent" << endl;
+    PRINT(learnRate, out);
+    PRINT(momentum, out);
+  }
 };
 
 #endif
